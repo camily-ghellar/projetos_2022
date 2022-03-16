6593d803d354d7a544afeeb8fbc440ccc6bdb7fc
camily-ghellar <ghellarcamily@gmail.com>
2022-03-16 20:21:22 -0300

    codigo que solicita nome, telefone e email. primeiro codigo para teste do git.

---
+++ exercicio1Git.py
@@ -0,0 +1,9 @@
+#made for Camily do Nascimento Ghellar
+
+nome= input("Digite seu nome: ")
+email= input("Digite seu e-mail: ")
+tel= input("Digite o seu telefone: ")
+
+print("Nome:", nome)
+print("Email:", email)
+print("Telefone:", tel)
